a852d2b51745627a984707daace7f8b9458b1bcb
Mikkel Kaas <mk@thy-it.com>
2019-03-02 18:31:28 +0000

    Script for data persistens added.

---
+++ setupPersistentDomain.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#set -x
+
+vDomain=$1
+container=$2
+
+showSyntax() {
+	echo "Thy-IT Setup Simple Container Script"
+	echo "------------------------------------"
+	echo "Syntax: $0 <Domain> <Container>     "
+	echo ""
+}
+
+if [ -z "$vDomain" ]
+then
+	showSyntax
+else
+	docker create volume $vDomain-data
+	docker run -ti --name $vDomain-SimpleHost --volume=$vDomain-data -e VIRTUAL_HOST=$vDomain -d $container
+fi
+
