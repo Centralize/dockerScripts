16b7d84f888e634d84b6c8028ccf7f261eba3c38
Mikkel Kaas <mikkel.kaas.64@gmail.com>
2023-11-09 10:45:20 +0100

    [BUGFIX] setupWordpress.sh, cleanNetwork, removeContainer

--- cleanNetwork.sh
+++ cleanNetwork.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 customer=$1
-HEXID=`docker network ls | grep "$customer" | cut -d ' ' -f 1`
+
+if [ -z "$customer" ]; then
+	echo "Syntax error:"
+	echo ""
+	echo "Syntax: $0 <CustomerName>"
+	echo ""
+	exit 1
+fi
+
+HEXID=`docker network ls | grep -i "$customer" | cut -d ' ' -f 1`
+
+if [ "$HEXID" == "" ]; then
+	echo "Customer Network not found."
+	exit 1
+fi
 
 docker network rm $HEXID
 docker network ls
+exit 0
--- setupWordpress.sh
+++ setupWordpress.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
+#set -x
+
 #INPUT VARS
 vDomain=$1
 CustomerName=$2
+newpass=$(pwgen -cnsB 16 1)
+port=$(sudo netstat -lnput|grep -i ":3306")
+
+if [ -z "$port" ]; then
+	mysqlPort=3306
+else
+	mysqlPort=3307
+fi
+
+function syntax()
+{
+   echo "Syntax:"
+   echo ""
+   echo "$0 <vDomain> <CustomerName>"
+   echo ""
+}
+
+if [ -z "$vDomain" ]; then
+	echo "Syntax Error: vDomain is missing."
+	echo ""; echo ""
+	syntax
+	exit 1
+fi
+
+if [ -z "$CustomerName" ]; then
+	echo "Syntax Error: Customer name is missing."
+	echo ""; echo ""
+	syntax
+	exit 1
+fi
 
 echo "Starting creation process for $CustomerName."
 
@@ -12,7 +44,8 @@ time docker network create $CustomerName-wordpress-net
 
 #SETUP MySQL Server
 echo "Creating MySQL Server for $CustomerName with access only on $CustomerName-wordpress-net."
-time docker run -ti --name $CustomerName-mysql --network $CustomerName-wordpress-net -e MYSQL_ROOT_PASSWORD=c7ad44cbad762a5da0a452f9 -p 3307:3306 -d mysql:latest
+time docker run -ti --name $CustomerName-mysql --network $CustomerName-wordpress-net -e MYSQL_ROOT_PASSWORD=$newpass -p $mysqlPort:3306 -d mysql:latest
+echo "MySQL root password: $newpass"
 
 #SETUP Wordpress Server
 echo "Creating Wordpress Server for $CustomerName with access from the Domain: $vDomain."
